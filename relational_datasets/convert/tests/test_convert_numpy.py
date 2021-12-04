29b985f63fb14bc4959277814ce64bd8082421c4
Alexander L. Hayes <alexander@batflyer.net>
2021-12-04 19:02:41 +0000

    ✅ Add tests for multiclass `convert_numpy`

--- relational_datasets/convert/tests/test_convert_numpy.py
+++ relational_datasets/convert/tests/test_convert_numpy.py
@@ -79,3 +79,38 @@ def test_convert_numpy_regression():
         "v3(+id,#varv3).",
         "v4(+id).",
     ]
+
+def test_convert_multiclass():
+    """Test converting a multiclass classification problem."""
+
+    X = numpy.array([[0, 1, 1], [1, 0, 2], [2, 2, 0], [1, 1, 1]])
+    y = numpy.array([0, 0, 1, 2])
+    data, modes = from_numpy(X, y)
+
+    assert data.pos == [
+        "v4(id1,0).",
+        "v4(id2,0).",
+        "v4(id3,1).",
+        "v4(id4,2).",
+    ]
+    assert data.neg == []
+    assert data.facts == [
+        "v1(id1,0).",
+        "v1(id2,1).",
+        "v1(id3,2).",
+        'v1(id4,1).',
+        "v2(id1,1).",
+        "v2(id2,0).",
+        "v2(id3,2).",
+        'v2(id4,1).',
+        "v3(id1,1).",
+        "v3(id2,2).",
+        "v3(id3,0).",
+        'v3(id4,1).'
+    ]
+    assert modes == [
+        "v1(+id,#varv1).",
+        "v2(+id,#varv2).",
+        "v3(+id,#varv3).",
+        "v4(+id,#classlabel).",
+    ]
